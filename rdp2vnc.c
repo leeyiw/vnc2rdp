dca4677a95faa25896a176c4038caa1e560a5e57
leeyiw <leeyiw@gmail.com>
2014-04-17 19:32:45 +0800

    add accept

--- rdp2vnc.c
+++ rdp2vnc.c
@@ -1,11 +1,15 @@
 #include <stddef.h>
 
+#include "rdp_conn.h"
 #include "rdp_server.h"
 
 int
 main(int argc, char *argv[])
 {
 	r2v_rdp_server_t *rdp_server = NULL;
+	r2v_rdp_conn_t *rdp_conn = NULL;
+	
 	rdp_server = r2v_rdp_server_init("0.0.0.0", 3389);
+	rdp_conn = r2v_rdp_server_accept(rdp_server);
 	return 0;
 }
